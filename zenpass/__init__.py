faa8efe6a7b0b9c4a71a4bff8e752657fc37c318
codesrg <srg.code@pm.me>
2022-11-05 00:28:23 +0530

    added __all__

--- zenpass/__init__.py
+++ zenpass/__init__.py
@@ -3,5 +3,10 @@
 __author__ = 'srg'
 __version__ = '1.0.4'
 
+__all__ = [
+    'PasswordGenerator',
+    'ZenpassException'
+]
+
 from .password import PasswordGenerator
 from .exception import ZenpassException
